e3f9c0fe7a3c5d42c9a4442eb08fcf7051aff00e
Carlos Sarmiento <carlosarmientopinto@gmail.com>
2025-03-24 10:36:05 -0500

    Commit iniciales

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,29 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import icon from "astro-icon";
+import tailwindcss from "@tailwindcss/vite";
 
 import cloudflare from '@astrojs/cloudflare';
 
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+  site: 'https://rcisas.com',
+  integrations: [mdx(), sitemap(),icon()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true
     }
   }),
+  experimental: {
+    svg: true,
+    responsiveImages: true,
+  },
+  image: {
+    remotePatterns: [{ protocol: "https" }],
+  }, 
+  vite: {
+        plugins: [tailwindcss()],
+      },
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Redes, Conexiones e Ingenierias';
+export const SITE_DESCRIPTION = 'Garantizamos protección y conectividad eficiente para su negocio. Nuestro compromiso con la innovación, calidad y transparencia nos permite ofrecer soluciones escalables y adaptadas a las necesidades de cada cliente.';
+export const SITE_URL = "rcisas.com";
