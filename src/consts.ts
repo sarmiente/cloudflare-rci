c8e93adb2ce696bf0d290b93ff0d92f46d215835
Carlos Sarmiento <carlosarmientopinto@gmail.com>
2026-04-09 15:21:33 -0500

    commitss de SEO

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = 'CCTV, Control de Acceso y Cableado Estructurado en Colombia | RCI SAS';
 export const SITE_DESCRIPTION = 'Instalamos CCTV, cámaras de seguridad, control de acceso, cableado estructurado y sistemas de detección de incendios en Cartagena y Barranquilla. Seguridad y conectividad profesional.';
 export const SITE_URL = "https://rcisas.com";
-export const SITE_IMAGE = "/cctv-control-acceso-cartagena-barranquilla.svg";
+
